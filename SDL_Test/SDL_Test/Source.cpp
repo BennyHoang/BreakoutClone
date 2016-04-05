e8948d734439f97911814d6f7cb40855e4037e50
Benny <hoaben13@student.westerdals.no>
2016-04-05 23:05:18 +0200

    SDL Tutorial #2

--- SDL_Test/SDL_Test/Source.cpp
+++ SDL_Test/SDL_Test/Source.cpp
@@ -1,35 +1,116 @@
 #include <SDL.h>
 #include <stdio.h>
 
+bool init();
+
+bool loadMedia();
+
+void close();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
 const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_HEIGHT = 400;
+
+
+bool init()
+{
+	bool success = true;
+
+	if(SDL_Init(SDL_INIT_VIDEO) < 0 )
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if(gWindow == NULL)
+		{
+			printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
+}
+
+bool loadMedia()
+{
+	bool success = true;
+	gHelloWorld = SDL_LoadBMP("SDL_Test/hello_world.bmp");
+	if(gHelloWorld == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "SDL_Test/hello_world.bmp", SDL_GetError());
+		success = false;
+	}
+	return success;
+}
+
+void close()
+{
+	SDL_Surface(gHellowWorld);
+	gHelloWorld = NULL;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
+}
 
 int main(int argc, char* args[])
 {
-	SDL_Window* window = NULL;
-
-	SDL_Surface* screenSurface = NULL;
-
-	if(SDL_Init(SDL_INIT_VIDEO)<0)
+	if(!init())
 	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}else
+		printf("Failed to initialize!\n");
+
+	}
+	else
 	{
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if(window == NULL)
+		if(!loadMedia())
 		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}else
+			printf("failed to load media!\n");
+
+		}
+		else
 		{
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(gWindow);
 			SDL_Delay(2000);
-			
-			
 		}
 	}
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	close();
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
