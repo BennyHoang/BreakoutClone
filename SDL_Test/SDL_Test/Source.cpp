df968035db05a6dee9615e7f6f48221b862a1fd2
Benny <hoaben13@student.westerdals.no>
2016-04-08 14:15:21 +0200

    added enums for Event listener

--- SDL_Test/SDL_Test/Source.cpp
+++ SDL_Test/SDL_Test/Source.cpp
@@ -4,6 +4,7 @@ and may not be redistributed without written permission.*/
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include <string>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -17,7 +18,6 @@ bool loadMedia();
 
 //Frees media and shuts down SDL
 void close();
-
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 
@@ -27,11 +27,25 @@ SDL_Surface* gScreenSurface = NULL;
 //The image we will load and show on the screen
 SDL_Surface* gXOut = NULL;
 
-bool init();
+enum KeyPressSurfaces
+{
+	KEY_PRESS_SURFACE_DEFAULT,
+	KEY_PRESS_SURFACE_UP,
+	KEY_PRESS_SURFACE_DOWN,
+	KEY_PRESS_SURFACE_LEFT,
+	KEY_PRESS_SURFACE_RIGHT,
+	KEY_PRESS_SURFACE_TOTAL
+};
 
-bool loadMedia();
 
-void close();
+SDL_Surface* loadSurface(std::string path);
+
+SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+
+SDL_Surface* gCurrentSurface = NULL;
+
+
+
 
 bool init()
 {
