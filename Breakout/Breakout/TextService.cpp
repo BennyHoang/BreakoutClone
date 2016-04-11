edaf6589e3f803de7a8b03be803827691f6c7097
Benny <hoaben13@student.westerdals.no>
2016-04-11 20:54:17 +0200

    added initialization of ttf

--- Breakout/Breakout/TextService.cpp
+++ Breakout/Breakout/TextService.cpp
@@ -1,1 +1,10 @@
 #include "TextService.h"
+
+bool TextService::init()
+{
+	if(TTF_Init() == 1)
+	{
+		return false;
+	}
+	return true;
+}
--- Breakout/Breakout/TextService.h
+++ Breakout/Breakout/TextService.h
@@ -4,3 +4,17 @@
 #include "SDL.h"
 #include "SDL_ttf.h"
 
+class TextService
+{
+public:
+	TextService();
+	~TextService();
+	bool init();
+private:
+	TTF_Font* m_font;
+	SDL_Surface* m_message;
+	SDL_Texture* m_text;
+	SDL_Rect m_textRect;
+	SDL_Color m_textcolor;
+
+};
