efc89cda751bc36238f1d0fcb0a79938d86397ea
Fredrik Hoel <hoefre14@student.westerdals.no>
2016-04-15 20:02:46 +0200

    sound updates

--- Breakout/Breakout/SoundManager.h
+++ Breakout/Breakout/SoundManager.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include <SDL.h>
+#include <SDL_mixer.h>
 
 class SoundManager {
 private:
+	// Background song
+	Mix_Music *bgm = Mix_LoadMUS("backgroundMusic.mp3");
+
+	// Sound effects
+	Mix_Chunk *seOne = Mix_LoadWAV("explo01.wav");
+	Mix_Chunk *seTwo = Mix_LoadWAV("explo02.wav");
+	Mix_Chunk *seThree = Mix_LoadWAV("explo03.wav");
 
 public:
 };
