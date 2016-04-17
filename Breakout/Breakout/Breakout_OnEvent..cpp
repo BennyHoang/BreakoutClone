b9ffe3a27a3e97f2f1e3841a6343620ff2953a55
OleF <olifredrik@gmail.com>
2016-04-17 21:34:37 +0200

    added uml

--- Breakout/Breakout/Breakout_OnEvent..cpp
+++ Breakout/Breakout/Breakout_OnEvent..cpp
@@ -5,42 +5,45 @@ void Breakout::OnEvent(SDL_Event* Event)
 	if (Event->type == SDL_QUIT) {
 		Running = false;
 	}
-
-	if (GameState.getInGame())
+	if (Event->type == SDL_KEYDOWN)
 	{
-		if (Event->key.keysym.sym == SDLK_LEFT ||
-			Event->key.keysym.sym == SDLK_a) {
-			posX -= 20;
-			if (posX < 0) posX = 0;
-			return;
-		}
 
-		if (Event->key.keysym.sym == SDLK_RIGHT ||
-			Event->key.keysym.sym == SDLK_d) {
-			posX += 20;
-			if (posX > SCREEN_WIDTH - 300) posX = SCREEN_WIDTH - 300;
-			return;
-		}
-
-		if (Event->key.keysym.sym == SDLK_SPACE && GameState.canShoot())
+		if (GameState.getInGame())
 		{
-			level.ball->updateVector(0, 1);
-			return;
+			if (Event->key.keysym.sym == SDLK_LEFT ||
+				Event->key.keysym.sym == SDLK_a) {
+				posX -= 30;
+				if (posX < 0) posX = 0;
+				return;
+			}
+
+			if (Event->key.keysym.sym == SDLK_RIGHT ||
+				Event->key.keysym.sym == SDLK_d) {
+				posX += 30;
+				if (posX > SCREEN_WIDTH - 300) posX = SCREEN_WIDTH - 300;
+				return;
+			}
+
+			if (Event->key.keysym.sym == SDLK_SPACE && GameState.canShoot())
+			{
+				level.ball->updateVector(0, 1);
+				return;
+			}
+		}
+
+
+
+
+		if (GameState.getInMenu())
+		{
+			if (Event->key.keysym.sym == SDLK_p)
+			{
+				GameState.setInMenu(false);
+				GameState.setFirstLevel(true);
+				GameState.setInGame(true);
+			}
 		}
 	}
 
-
-
-
-	if (GameState.getInMenu())
-	{
-		if (Event->key.keysym.sym == SDLK_p)
-		{
-			GameState.setInMenu(false);
-			GameState.setFirstLevel(true);
-			GameState.setInGame(true);
-		}
-	}
-
-
+	//SDL_PumpEvents();
 }
